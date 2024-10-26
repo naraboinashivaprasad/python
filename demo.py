a7ba11673fcb987b0e57b4d778245c326bc603c0
naraboinashivaprasad <shivaprasad89830@gmail.com>
2024-10-26 18:36:16 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,51 @@
+#print("this is my first program")
+'''
+a=10
+b=30
+print(a)
+print(type(a))
+print(int(a))
+'''
+#this is a integer
+#a=10 #int
+
+
+#variables
+'''
+shivaPrasad   #camel case
+shiva_prasad  #snake case
+ShivaPrasad   #pascal case
+'''
+
+#type conversions
+#a=100
+#print(float(a))        #implict conversion
+
+#a=20.333
+#print(int(a))                #explicit
+
+#dynamic
+
+#a=input("enter the something:")
+#print(a)
+
+
+
+
+
+#simple interest
+
+s=15000
+t=90
+r=30
+si=s*t*r/100
+print(si)
+
+#compound interest
+p=10000
+r=5
+n=1000
+t=100
+
+ci=p(1+r/n)**nt
+print(ci)
---
+++ list.py
@@ -0,0 +1,13 @@
+#shiva=[]
+
+#print(type(shiva))
+
+
+#krishna=[1,23,22.3,"shiva"]  #1
+#krishna1=list([1,2,3,45,6]) #2
+#print(krishna)
+#print(krishna1)
+
+
+krishna=[1,2,3,4,5,6,7,8,9,6]
+print(krishna[2])
